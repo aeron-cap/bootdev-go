702518bab86f2054a57a407c09991ff8e1729060
aeron-cap <aeroncaponpon.01@gmail.com>
2026-02-22 17:40:16 +0800

    chapter 2

---
+++ chapter2/hello.go
@@ -0,0 +1,43 @@
+package main
+
+
+/*
+CHAPTER 2
+
+if INITIAL_STATEMENT; CONDITION {}
+eg.
+if length := getLenth(email); length < 10 {}
+
+
+switch var {
+case "condition1":
+ 	...
+case "condition2":
+	...
+default:
+	...
+}
+break statement is implicit in Go
+
+func NAME(param1 TYPE, param2 TYPE) {}
+
+declaration syntax is a bit different - types are on the right
+x int
+p *int
+
+variables in  Go are passed by value
+
+ignore return values
+x, _ := getValue()
+
+return statement without arguments returns the names return values (naked return)
+
+defers are  a unique feature in  Go, it allows a function to run before
+its enclosing function returns
+last in first out basis in execution
+
+go is block-scoped meaning everything inside {} is scoped
+
+currying is a concept from functional programming and involces partial application of functions
+
+ */
