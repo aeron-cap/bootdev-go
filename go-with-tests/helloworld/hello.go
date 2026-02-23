b78b4c56898cbbd334922c55ef9676487dca26fc
aeron-cap <aeroncaponpon.01@gmail.com>
2026-02-23 23:57:12 +0800

    learning

--- go-with-tests/helloworld/hello.go
+++ go-with-tests/helloworld/hello.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"di"
-	"log"
-	"net/http"
+	// "di"
+	"fmt"
+	// "log"
+	// "net/http"
 	// "os"
 )
 
@@ -21,7 +22,17 @@ func Hello(name, language string) string {
 	return prefix + name
 }
 
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, b+a
+		return a
+	}
+}
+
 func main() {
 	// di.Greet(os.Stdout, Hello("Aeron",  "English"))
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(di.MyGreeterHandler)))
+	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(di.MyGreeterHandler)))
+	a := fib()
+	fmt.Println(a(), a(), a(), a())
 }
